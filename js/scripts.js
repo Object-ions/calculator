f81b5ff30e471f9b30917a8be549733aedb278c6
objct-ions <moshikoatia@gmail.com>
2023-05-30 16:11:58 -0700

    combined all functions into a single function that works and give you one output to console.log w all the nswers at once

--- js/scripts.js
+++ js/scripts.js
@@ -1,31 +1,35 @@
-function saySomething(whatToSay) {
-    window.alert(whatToSay);
-}
+console.log("test")
+
+// user interface logic 
+let number1 = parseInt(prompt(" to add, subtruct, multiply and divide; Enter a number:"));
+let number2 = parseInt(prompt("Enter another number:"));
+
+// }
 // adding function
 function add(number1, number2) {
-    return number1 + number2;
-}
+    let twoNum = number1 + number2;
+    console.log("The addition of your numbers equals " + twoNum + ".");
+} 
 
 // subtructing fuction
 function sub(number1, number2) {
-    return number1 - number2;
+    twoNum = number1 - number2;
+    console.log("The subtraction of your numbers equals " + twoNum + ".");
 }
 
 // subtructing fuction
 function multiply(number1, number2) {
-    return number1 * number2;
+    twoNum = number1 * number2;
+    console.log("The multiply of your numbers equals " + twoNum + ".");
 }
 
 // subtructing fuction
 function divide(number1, number2) {
-    return number1 / number2;
+    twoNum = number1 / number2;
+    console.log("The division of your numbers equals " + twoNum + ".");
 }
 
-  // user interface logic 
-let number1 = parseInt(prompt(" to add, subtruct, multiply and divide; Enter a number:"));
-let number2 = parseInt(prompt("Enter another number:"));
-
-window.alert(add(number1, number2));
-window.alert(sub(number1, number2));
-window.alert(multiply(number1, number2));
-window.alert(divide(number1, number2));
+add(number1, number2);
+sub(number1, number2);
+multiply(number1, number2);
+divide(number1, number2);
