1c7417590bdf9f1048019664f24e266bb13f4321
objct-ions <moshikoatia@gmail.com>
2023-05-30 16:01:05 -0700

    fixed all functions to work, changed the output from 'alert' to consol.log

--- js/scripts.js
+++ js/scripts.js
@@ -1,18 +1,31 @@
 function saySomething(whatToSay) {
     window.alert(whatToSay);
 }
-
+// adding function
 function add(number1, number2) {
     return number1 + number2;
 }
 
-// business logic
-function add(number1, number2) {
-    return number1 + number2;
-  }
-  
+// subtructing fuction
+function sub(number1, number2) {
+    return number1 - number2;
+}
+
+// subtructing fuction
+function multiply(number1, number2) {
+    return number1 * number2;
+}
+
+// subtructing fuction
+function divide(number1, number2) {
+    return number1 / number2;
+}
+
   // user interface logic 
-  const number1 = parseInt(prompt("Enter a number:"));
-  const number2 = parseInt(prompt("Enter another number:"));
-  
-  window.alert(add(number1, number2));
+let number1 = parseInt(prompt(" to add, subtruct, multiply and divide; Enter a number:"));
+let number2 = parseInt(prompt("Enter another number:"));
+
+window.alert(add(number1, number2));
+window.alert(sub(number1, number2));
+window.alert(multiply(number1, number2));
+window.alert(divide(number1, number2));
