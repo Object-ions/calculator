eecf5b395b109e20edb2fd13ee1935ae310f5820
objct-ions <moshikoatia@gmail.com>
2023-05-30 16:32:27 -0700

    added user interface and buissness logic, made it all work and console.log output. all bugs fixed.

--- js/scripts.js
+++ js/scripts.js
@@ -12,13 +12,22 @@ function arithmetics(num1, num2) {
     sub = number1 - number2;
     mul = number1 * number2;
     div = number1 / number2;
-    //output to console
+    //output to console user interface:
     console.log("The addition of your numbers equals " + add + "." +
                  "The subtraction of your numbers equals " + sub + "." +
                  "The multiply of your numbers equals " + mul + "." +
                  "The dividis of your numbers equals " + div + ".");
+
+    //output to console buisness logic:
+    console.log(
+        number1 + "+" + number2 + "=" + add + " ," +
+        number1 + "-" + number2 + "=" + sub + " ," +
+        number1 + "*" + number2 + "=" + mul + " ," +
+        number1 + "/" + number2 + "=" + div
+    );
+
 }
 
 arithmetics(number1, number2)
 
-// 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
+// 
